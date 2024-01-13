e74c59153204f27617cfa9f5ed5fbe74bb7bd02a
Jan <schnock.art@gmail.com>
2024-01-13 17:39:12 -0300

    Updates

---
+++ static/js/420.bc91de17.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkportfolio=self.webpackChunkportfolio||[]).push([[420],{420:(o,e,s)=>{s.r(e),s.d(e,{default:()=>t});const t={}}}]);
+//# sourceMappingURL=420.bc91de17.chunk.js.map
